fc3412110454c2e204b0e70a5468569277866437
gracece <guolyuan@gmail.com>
2012-12-22 21:44:34 +0800

    vieworder usign array

--- vieworders.php
+++ vieworders.php
@@ -5,23 +5,46 @@
   <title>vier order</title>
 </head>
 <body>
-  
-<?php 
-$fp = @fopen('order.txt','rb');
-flock($fp,LOCK_SH);
-if (!$fp) {
-  echo "No order now!";
-  exit;
+<?php  
+$order = file("./order.txt");
+$number = count($order);
+if ($number == 0) {
+  echo "NO ORDER";
 }
 
-while (!feof($fp))
-{
-  $order = fgets($fp,999);
-  echo $order."<br />";
+echo ' <table>
+    <tr>
+  <th>Date</th>
+  <th>TIR</th>
+  <th>OIL</th>
+  <th>SPA</th>
+  <th>TOTAL</th>
+  <th>ADDRESS</th>
+    </tr> ';
+for ($i=0;i<$number;$i++>) { 
+  $line = explode ('\t',$order[$i]);
+
+  //对数字进行转换
+  $line[1] = intval($line[1]);
+  $line[2] = intval($line[2]);
+  $line[3] = intval($line[3]);
+
+
+  echo ' <tr>
+      <td>'.$line[0].'</td>
+      <td>'.$line[1].'</td>
+      <td>'.$line[2].'</td>
+      <td>'.$line[3].'</td>
+      <td>'.$line[4].'</td>
+      <td>'.$line[5].'</td>
+    </tr> ';
 }
 
-flock($fp,LOCK_UN);
-fclose($fp);
+echo '</table>';
+
+
+}
+
 
 ?>
 </body>
